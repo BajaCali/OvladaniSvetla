b15a19882ee8471b39fd9acf4b19c667fdaf9668
BajaCali <michaelnemec47@gmail.com>
2017-09-16 13:37:47 +0200

    Add changing via button or switch

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #define LIGHT  23
 #define CONNECTED_LED  13
 #define RESET_PIN 21
+#define interruptPin 4
 
 #define _GLIBCXX_USE_C99 1
 #include <stdio.h>
@@ -34,15 +35,24 @@ esp_err_t hndl(void* x, system_event_t* e)
 
 WiFiServer server(80);
 
+volatile unsigned long timeHigh; 
+volatile unsigned long minDifTime = 100;
+
 // Client variables 
 char linebuf[80];
 int charcount = 0;
 
 void reset();
 void WiFi_setup();
+String main_page();
+void change_light();
+void A_button();
+void A_switch();
 
 void setup() {
     Serial.begin(SERIAL_BAUDRATE);
+	pinMode(interruptPin, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(interruptPin), A_switch, CHANGE);
 	pinMode(RESET_PIN, OUTPUT);
 	pinMode(LIGHT, OUTPUT);
 	digitalWrite(RESET_PIN, HIGH);
@@ -66,6 +76,7 @@ void loop(){
 		boolean currentLineIsBlank = true;
 		while (client.connected()) {
 			if (client.available()) {
+				// Serial.print("\navailable: ");
 				char c = client.read();
 				Serial.write(c);
 				//read char by char HTTP request
@@ -74,35 +85,17 @@ void loop(){
 				// if you've gotten to the end of the line (received a newline
 				// character) and the line is blank, the http request has ended,
 				// so you can send a reply
+				// Serial.print("new char readed. ");
 				if (c == '\n'){
 					if(strstr(linebuf,"GET /on_off")){
-                        if(digitalRead(LIGHT) == LOW){
-                            digitalWrite(LIGHT, HIGH);
-                            Serial.println("on");
-                        }
-                        else {
-                            digitalWrite(LIGHT, HIGH);
-                            Serial.println("off");
-                        }
+						change_light();
 				    }
 				}
+				// Serial.print("available changes changed. ");
 				if (c == '\n' && currentLineIsBlank) {
 					// send a standard http response header
-                    String Page;
-                    Page += "\nHTTP/1.1 200 OK";
-                    Page += "\nContent-Type: text/html";
-                    Page += "\nConnection: close";  // the connection will be closed after completion of the response
-                    Page += "\n";
-                    Page += "\n<!DOCTYPE HTML><html><head>";
-                    Page += "\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>";
-                    Page += "\n<link rel=\"stylesheet\" type=\"text/css\" href=\"css.css\">";
-                    Page += "\n<style type=\"text/css\">";
-                    Page += "\nbutton {background-color: white; color: black; border: 2px solid #555555; border-radius: 4px}";
-                    Page += "\nbutton:hover {background-color: #555555; color: white;border-radius: 4px}";
-                    Page += "\n</style>";
-                    Page += "\n<title>The Light</title>";
-                    Page += "\n<p><a href=\"on_off\"><button style=\"position: absolute; left: 25%; width: 50%; height: auto\"><b>CHANGE</b></button></a></p>";
-    				client.print(Page);
+					client.print(main_page());
+					break;
 				}
 				if (c == '\n') {
 					// you're starting a new 
@@ -115,6 +108,7 @@ void loop(){
 					// you've gotten a character on the current line
 					currentLineIsBlank = false;
 				}
+				// Serial.print("control \\r and \\n done.");
 			}
 		}
 		// give the web browser time to receive the data
@@ -157,4 +151,53 @@ void WiFi_setup(){
 
 void reset(){
 	digitalWrite(RESET_PIN, LOW);
+}
+
+String main_page(){
+	String Page;
+	Page += "\nHTTP/1.1 200 OK";
+	Page += "\nContent-Type: text/html";
+	Page += "\nConnection: close";  // the connection will be closed after completion of the response
+	Page += "\n";
+	Page += "\n<!DOCTYPE HTML><html><head>";
+	Page += "\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>";
+	Page += "\n<link rel=\"stylesheet\" type=\"text/css\" href=\"css.css\">";
+	Page += "\n<style type=\"text/css\">";
+	Page += "\nbody {background: black}";
+	Page += "\nbutton {background-color: white; color: black; border: 2px solid #555555; border-radius: 4px}";
+	Page += "\nbutton:hover {background-color: #555555; color: white;border-radius: 4px}";
+	Page += "\n</style>";
+	Page += "\n<title>The Light</title>";
+	Page += "\n<p><a href=\"on_off\"><button style=\"position: absolute; left: 25%; width: 50%; height: 20%\"><b>CHANGE</b></button></a></p>";
+	Page += "\n</html>";
+	return Page;
+}
+
+void change_light(){
+	if(digitalRead(LIGHT) == LOW){
+		digitalWrite(LIGHT, HIGH);
+		Serial.println("on");
+	}
+	else {
+		digitalWrite(LIGHT, LOW);
+		Serial.println("off");
+	}
+}
+
+void A_button(){
+	if(digitalRead(interruptPin) == LOW){
+	  timeHigh = millis();
+	}
+	else{
+		if((millis() - timeHigh) > minDifTime){
+		change_light();  
+		}
+	} 
+}
+  
+void A_switch(){
+	if((millis() - timeHigh) > minDifTime){
+		change_light();  
+	}
+	timeHigh = millis();
 }
